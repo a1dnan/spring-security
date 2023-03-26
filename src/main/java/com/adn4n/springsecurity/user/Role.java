f6dabfd856f8aaa03180dd1b17f652765f1120fb
a1dnan <aaliladnane@gmail.com>
2023-03-26 03:02:43 +0000

    Add User Entity and AuthenticationController

---
+++ src/main/java/com/adn4n/springsecurity/user/Role.java
@@ -0,0 +1,6 @@
+package com.adn4n.springsecurity.user;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/com/adn4n/springsecurity/user/User.java
@@ -0,0 +1,26 @@
+package com.adn4n.springsecurity.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
