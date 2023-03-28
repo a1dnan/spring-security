99ab27c2841e89a49c7c4b6002ae0d82c34bf34f
a1dnan <aaliladnane@gmail.com>
2023-03-28 09:44:09 +0000

    Update Authentication Controller

--- src/main/java/com/adn4n/springsecurity/auth/AuthenticationController.java
+++ src/main/java/com/adn4n/springsecurity/auth/AuthenticationController.java
@@ -1,6 +1,9 @@
 package com.adn4n.springsecurity.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResposne> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResposne> register(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
 }
