a98a5e83cb59f0baa6cbc4324d0be1bdc5affd0a
a1dnan <aaliladnane@gmail.com>
2023-04-26 23:58:40 +0100

    Update Authentication Service

--- src/main/java/com/adn4n/springsecurity/auth/AuthenticationService.java
+++ src/main/java/com/adn4n/springsecurity/auth/AuthenticationService.java
@@ -2,6 +2,9 @@ package com.adn4n.springsecurity.auth;
 
 
 import com.adn4n.springsecurity.config.JwtService;
+import com.adn4n.springsecurity.token.Token;
+import com.adn4n.springsecurity.token.TokenRepo;
+import com.adn4n.springsecurity.token.TokenType;
 import com.adn4n.springsecurity.user.Role;
 import com.adn4n.springsecurity.user.User;
 import com.adn4n.springsecurity.user.UserRepo;
@@ -17,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
 
     private final UserRepo userRepository;
+    private final TokenRepo tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
@@ -29,10 +33,21 @@ public class AuthenticationService {
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER)
                 .build();
-        userRepository.save(user);
+        var savedUser = userRepository.save(user);
         var jwtToken = jwtService.generateToken(user);
+        saveUserToken(savedUser, jwtToken);
         return AuthenticationResposne.builder()
                 .token(jwtToken)
+                .build();
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
                 .build();
     }
 
@@ -46,7 +61,7 @@ public class AuthenticationService {
         var user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
         var jwtToken = jwtService.generateToken(user);
-
+        saveUserToken(user,jwtToken);
         return AuthenticationResposne.builder()
                 .token(jwtToken)
                 .build();
